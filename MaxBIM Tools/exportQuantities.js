6196b5dda79e8ea24370b3b7bdcbc1e7fc41923a
Soonbum Jeong <peacemaker84@gmail.com>
2021-11-09 12:27:09 +0900

    MaxBIM Tools (2021.11.09 - 12:26)
    수정됨
    1. 텍스트 파일의 본문 가져오기
    2. 엑셀 파일 생성 및 기록하기

--- MaxBIM Tools/exportQuantities.js
+++ MaxBIM Tools/exportQuantities.js
@@ -1,4 +1,3 @@
-// 제목, 날짜 정보를 가져옴
 const fileButton = document.getElementById ("file");
 const titleInput = document.getElementById ("title");
 const dateInput = document.getElementById ("date");
@@ -7,23 +6,42 @@ const saveButton = document.getElementById ("save");
 saveButton.addEventListener ("click", showValue);
 
 function showValue () {
+    // 타이틀, 날짜 정보를 가져옴
     console.log (`제목: ${titleInput.value}, 날짜: ${dateInput.value}`);
 
     const selectedFiles = fileButton.files;
 
     for (const file of selectedFiles) {
         if (file.type == "text/plain") {
+            console.log (`파일명: ${file.name}\n`);
+
             let reader = new FileReader ();
-            let bodyText = "";
-            reader.onload = function () {
-                bodyText = reader.result;
+            reader.onload = function (ev) {
+                console.log (ev.target.result);
             };
             reader.readAsText (file, "euc-kr");
-            console.log (`파일명: ${file.name}\n`);
-            console.log (bodyText);
+
+            var wb = XLSX.utils.book_new ();    // workbook 생성
+            wb.SheetNames.push ("Sheet 1");     // 시트 생성
+            
+            var wsData = [[],['A1' , 'A2', 'A3'],['B1','B2','B3'],['C1','C2']];    // 기록할 데이터 !!!
+
+            // 데이터를 엑셀 파일로 저장함
+            var ws = XLSX.utils.aoa_to_sheet (wsData);
+            wb.Sheets ["Sheet 1"] = ws;
+            var wbout = XLSX.write (wb, {bookType:'xlsx', type:'binary'});
+            saveAs (new Blob ([s2ab (wbout)], {type:"application/octet-stream"}), "예제.xlsx");
         }
     }
 }
+
+function s2ab (s) {
+    var buf = new ArrayBuffer (s.length);
+    var view = new Uint8Array (buf);
+    for (var i=0 ; i < s.length ; i++) view [i] = s.charCodeAt(i) & 0xFF;
+    return buf;
+}
+
 
 // 예제: 텍스트 파일 보여주기
 /*
