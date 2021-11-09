18c67f98e21caae430c54d915a1c5cfe28aada08
Soonbum Jeong <peacemaker84@gmail.com>
2021-11-09 14:57:53 +0900

    MaxBIM Tools (2021.11.09 - 14:55)
    수정됨
    1. 파일명 자르기 및 확장자 변경
    2. 엑셀 파일 내용 추가하기

--- MaxBIM Tools/exportQuantities.js
+++ MaxBIM Tools/exportQuantities.js
@@ -3,103 +3,60 @@ const titleInput = document.getElementById ("title");
 const dateInput = document.getElementById ("date");
 const saveButton = document.getElementById ("save");
 
-saveButton.addEventListener ("click", showValue);
+saveButton.addEventListener ("click", saveXLSX);
 
-function showValue () {
+function saveXLSX () {
     // 타이틀, 날짜 정보를 가져옴
-    console.log (`제목: ${titleInput.value}, 날짜: ${dateInput.value}`);
+    //console.log (`제목: ${titleInput.value}, 날짜: ${dateInput.value}`);
 
     const selectedFiles = fileButton.files;
 
     for (const file of selectedFiles) {
         if (file.type == "text/plain") {
-            console.log (`파일명: ${file.name}\n`);
+            // ================================================== 파일명 읽어오기
+            //console.log (`파일명: ${file.name}\n`);
+            let excelFileName = file.name.replace (/.txt/gi, '.xlsx');
+            excelFileName = excelFileName.replace (' - 선택한 부재 정보 (중간보고서)', '');
+            excelFileName = excelFileName.replace (' - Graphisoft ArchiCAD-64 19', '');
 
+            // ================================================== 텍스트 파일 읽어오기
+            let textContents;
             let reader = new FileReader ();
             reader.onload = function (ev) {
-                console.log (ev.target.result);
+                textContents = ev.target.result;    // 텍스트 파일 내용 저장
+
+                // 엑셀 파일 생성 (SheetJS 무료 버전에서는 셀 스타일 지정은 불가함)
+                let wb = XLSX.utils.book_new ();    // workbook 생성
+                wb.SheetNames.push ("Sheet 1");     // 시트 생성
+                
+                // 기록할 데이터는 2차원 배열로 입력하면 됨: let wsData = [[],['A1' , 'A2', 'A3'],['B1','B2','B3'],['C1','C2']];  // 예제
+
+                // 실제 기록할 데이터
+                let wsData = [ [titleInput.value], [], ['','','','','','',dateInput.value], ['구간', '품목', '규격', '길이', '수량', '단위', '비고'] ];
+                wsData.push (['텍스트 1']);
+                wsData.push (['텍스트 2']);
+                // let strArray = textContents.split ('\n');
+                // for (let i=0 ; i < strArray.length ; i++)
+                //     strArray [i] = strArray [i].trim ();
+                // let wsData = [  [strArray [0], strArray [1], strArray [2], strArray [3], strArray [4]],
+                //                 [strArray [5], strArray [6], strArray [7], strArray [8], strArray [9]], ];
+
+                // 데이터를 엑셀 파일로 저장함
+                let ws = XLSX.utils.aoa_to_sheet (wsData);
+                wb.Sheets ["Sheet 1"] = ws;
+                wb ["Sheets"]["Sheet 1"]["!cols"] = [{wpx: 40}, {wpx: 150}, {wpx: 110}, {wpx: 80}, {wpx: 80}, {wpx: 80}, {wpx: 80}];    // 열 너비 지정
+                let wbout = XLSX.write (wb, {bookType:'xlsx', type:'binary'});
+                saveAs (new Blob ([s2ab (wbout)], {type:"application/octet-stream"}), excelFileName);
             };
             reader.readAsText (file, "euc-kr");
-
-            var wb = XLSX.utils.book_new ();    // workbook 생성
-            wb.SheetNames.push ("Sheet 1");     // 시트 생성
-            
-            var wsData = [[],['A1' , 'A2', 'A3'],['B1','B2','B3'],['C1','C2']];    // 기록할 데이터 !!!
-
-            // 데이터를 엑셀 파일로 저장함
-            var ws = XLSX.utils.aoa_to_sheet (wsData);
-            wb.Sheets ["Sheet 1"] = ws;
-            var wbout = XLSX.write (wb, {bookType:'xlsx', type:'binary'});
-            saveAs (new Blob ([s2ab (wbout)], {type:"application/octet-stream"}), "예제.xlsx");
         }
     }
 }
 
+// 파일 저장 루틴
 function s2ab (s) {
-    var buf = new ArrayBuffer (s.length);
-    var view = new Uint8Array (buf);
-    for (var i=0 ; i < s.length ; i++) view [i] = s.charCodeAt(i) & 0xFF;
+    let buf = new ArrayBuffer (s.length);
+    let view = new Uint8Array (buf);
+    for (let i=0 ; i < s.length ; i++) view [i] = s.charCodeAt(i) & 0xFF;
     return buf;
 }
-
-
-// 예제: 텍스트 파일 보여주기
-/*
-const input = document.getElementById ('file_uploads');     // input 태그 (file 타입) 가져옴
-const preview = document.getElementById ('preview');        // div 태그 가져옴
-
-input.addEventListener ('change', showTextFile);    // input 태그에 change 이벤트 리스너 부착, change 발생시 showTextFile 실행
-
-function showTextFile () {
-    const selectedFiles = input.files;              // 파일 (여러 개일 수 있음)
-    const list = document.createElement ('ul');     // 목록
-    preview.appendChild (list);
-
-    // 파일마다 체크
-    for (const file of selectedFiles) {
-        const listItem = document.createElement ('li');     // 목록 내 항목
-
-        // 파일 타입이 text/plain일 경우에만 유효함
-        if (validFileType (file)) {
-            const summary = document.createElement ('div');
-            summary.textContent = `파일명 : ${file.name}, 파일 크기 : ${returnFileSize (file.size)}.`;
-
-            const textContents = document.createElement ('div');
-
-            // 파일 열자마자 reader의 결과를 textContents의 텍스트로 세트 !!!
-            let reader = new FileReader ();
-            reader.onload = function () {
-                textContents.innerText = reader.result;
-            };
-            reader.readAsText (file, "euc-kr");     // file을 인코딩 euc-kr로 읽어들임
-
-            listItem.appendChild (summary);
-            listItem.appendChild (textContents);
-        } else {
-            const message = document.createElement ('div');
-            message.textContent = `파일명 ${file.name}: .txt 파일을 선택하세요.`;
-            listItem.appendChild (message);
-        }
-
-        list.appendChild (listItem);
-    }
-}
-
-const fileTypes = [
-    'text/plain',
-];
-
-function validFileType (file) {
-    return fileTypes.includes (file.type);
-}
-
-function returnFileSize (number) {
-    if (number < 1024) {
-        return number + 'bytes';
-    } else if (number > 1024 && number < 1048576) {
-        return (number/1024).toFixed(1) + 'KB';
-    } else if (number > 1048576) {
-        return (number / 1048576).toFixed(1) + 'MB';
-    }
-}
-*/
